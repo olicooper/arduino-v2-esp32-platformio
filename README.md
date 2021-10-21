6b34196791e8e63fa9f2e2c41bad832ff6028806
Oliver Cooper <oliver.d.cooper@gmail.com>
2021-10-21 14:06:06 +0100

    Add mqtt test code

--- README.md
+++ README.md
@@ -1,14 +1,46 @@
 ### Current status
 
-Simple project builds and runs Arduino v2.0.0 (Serial output confirms ESP-IDF v4.4 is running).
+Simple project builds and runs Arduino v2.0.0 (Serial output confirms ESP-IDF v4.4 is running) on a `lolin 32 Lite`. 
+However, the MQTT client fails to disconnect gracefully. The same error occurs on the [`mem-optimized` branch](https://github.com/espressif/arduino-esp32/tree/mem-optimized).
 
-### Additional info
+### Build log
 
-1. `C:\Users\<username>\.platformio\packages` includes:
-    * framework-arduinoespressif32 (v2.0.0)
-    * tool-cmake (v3.16.4)
-    * tool-idf (v1.0.1)
-    * toolchain-esp32ulp (v1.22851.191205)
-    * toolchain-xtensa-esp32 (v8.4.0+2021r1)
-    * toolchain-xtensa32 (v2.80400.210211)
-    * tool-esptoolpy (v1.30100.210531)
+```log
+PLATFORM: Espressif 32 (3.3.1+sha.3784198) > WEMOS LOLIN32 Lite
+HARDWARE: ESP32 240MHz, 320KB RAM, 4MB Flash
+PACKAGES:
+ - framework-arduinoespressif32 0.0.0+sha.44c1198
+ - tool-esptoolpy 1.30100.210531 (3.1.0)
+ - tool-mkspiffs 2.230.0 (2.30)
+ - toolchain-xtensa-esp32 8.4.0+2021r1
+Dependency Graph
+|-- <WiFi> 2.0.0
+RAM:   [=         ]  10.1% (used 32948 bytes from 327680 bytes)
+Flash: [=======   ]  69.1% (used 905993 bytes from 1310720 bytes)
+```
+
+### Debug log
+
+```log
+========= BOOT INFO =========
+
+MAC: FF:FF:FF:FF:FF:FF
+BootPart: offset=0x10000 size=0x140000
+IDF version: v4.4-dev-2313-gc69f0ec32
+Free memory: 222856 bytes
+
+=============================
+
+mqtt null 0
+WIFI CONNECTED
+wifconn mem: f=136636
+mqtt 0
+mqttconn mem: f=130124
+connend mem: f=130124
+MQTTEV MQTT_EVENTS, event_id=7
+Other event id:7
+MQTTEV MQTT_EVENTS, event_id=1
+MQTT_EVENT_CONNECTED
+mem: 109092 bytes
+mqtt stop 0
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,81 @@
-// #include <stdio.h>
-// #include <freertos/FreeRTOS.h>
-// #include <freertos/task.h>
-// #include "sdkconfig.h"
 extern "C" {
 #include "esp_ota_ops.h"
 }
 #include <Arduino.h>
+#include <WiFi.h>
+#include "mqtt_client.h"
+
+#define BUTTON_PIN 17
+
+//extern const uint8_t server_cert_pem_start[] asm("_binary_cert_pem_start");
+//extern const uint8_t server_cert_pem_end[] asm("_binary_cert_pem_end");
+
+esp_mqtt_client_handle_t mqttClient;
+bool wifiStarted = false;
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        Serial.printf("Last error %s: 0x%x\n", message, error_code);
+    }
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    Serial.printf("MQTTEV %s, event_id=%d\n", base, event_id);
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id = 0;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        Serial.printf("MQTT_EVENT_CONNECTED\n");
+        Serial.printf("mem: %d bytes\n", esp_get_free_heap_size());
+        // msg_id = esp_mqtt_client_publish(client, "topic/qos1", "data_3", 0, 1, 0);
+        // Serial.printf("sent publish successful, msg_id=%d\n", msg_id);
+
+        // msg_id = esp_mqtt_client_subscribe(client, "topic/qos0", 0);
+        // Serial.printf("sent subscribe successful, msg_id=%d\n", msg_id);
+
+        // msg_id = esp_mqtt_client_subscribe(client, "topic/qos1", 1);
+        // Serial.printf("sent subscribe successful, msg_id=%d\n", msg_id);
+
+        // msg_id = esp_mqtt_client_unsubscribe(client, "topic/qos1");
+        // Serial.printf("sent unsubscribe successful, msg_id=%d\n", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        Serial.printf("MQTT_EVENT_DISCONNECTED\n");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        Serial.printf("MQTT_EVENT_SUBSCRIBED, msg_id=%d\n", event->msg_id);
+        // msg_id = esp_mqtt_client_publish(client, "topic/qos0", "data", 0, 0, 0);
+        // Serial.printf("sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        Serial.printf("MQTT_EVENT_UNSUBSCRIBED, msg_id=%d\n", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        Serial.printf("MQTT_EVENT_PUBLISHED, msg_id=%d\n", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        Serial.printf("MQTT_EVENT_DATA\n");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        Serial.printf("MQTT_EVENT_ERROR\n");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls\n", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack\n", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno\n",  event->error_handle->esp_transport_sock_errno);
+            Serial.printf("Last errno string (%s)\n", strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        break;
+    default:
+        Serial.printf("Other event id:%d\n", event->event_id);
+        break;
+    }
+}
 
 void setup() {
     Serial.begin(115200);
@@ -27,8 +97,54 @@ void setup() {
     Serial.printf("IDF version: %s\n", esp_get_idf_version());
     Serial.printf("Free memory: %d bytes\n", esp_get_free_heap_size());
     Serial.println("\n=============================\n");
+
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+    WiFi.mode(WIFI_MODE_STA);
+
+    esp_mqtt_client_config_t mqtt_cfg = {};
+    mqtt_cfg.uri = "ws://host/ws";
+    mqtt_cfg.client_id = "clientid";
+    mqtt_cfg.buffer_size = 256;
+    mqtt_cfg.network_timeout_ms = 5000;
+    mqtt_cfg.keepalive = 60;
+    //mqtt_cfg.cert_pem = (const char *)server_cert_pem_start;
+    
+    mqttClient = esp_mqtt_client_init(&mqtt_cfg);
+    Serial.printf("mqtt null %u\n", mqttClient == NULL);
+    
+    esp_mqtt_client_register_event(mqttClient, (esp_mqtt_event_id_t)ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+
+    WiFi.begin("ssid", "password");
 }
 
 void loop() {
-    delay(5000);
+    if (!wifiStarted) {
+        while (!WiFi.isConnected()) {
+            delay(500);
+        }
+
+        {
+            wifiStarted = true;
+            Serial.printf("WIFI CONNECTED\n");
+            Serial.printf("wifconn mem: f=%d\n", esp_get_free_heap_size());
+            delay(1000);
+
+            auto success = esp_mqtt_client_start(mqttClient);
+            Serial.printf("mqtt %d\n", success);
+            Serial.printf("mqttconn mem: f=%d\n", esp_get_free_heap_size());
+        }
+        Serial.printf("connend mem: f=%d\n", esp_get_free_heap_size());
+        return;
+    }
+
+    delay(2000);
+
+    if (mqttClient != NULL && digitalRead(BUTTON_PIN) == LOW)
+    {
+        auto success = esp_mqtt_client_stop(mqttClient);
+        // auto success = esp_mqtt_client_disconnect(mqttClient);
+        Serial.printf("mqtt stop %d\n", success);
+        delay(3000);
+    }
 }
