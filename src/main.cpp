7fcd6cb5e5835d96e7ba50df0cffca42c64723ac
Oliver Cooper <oliver.d.cooper@gmail.com>
2021-10-21 16:17:18 +0100

    Update mqtt code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 extern "C" {
 #include "esp_ota_ops.h"
+#include "esp_log.h"
 }
 #include <Arduino.h>
 #include <WiFi.h>
 #include "mqtt_client.h"
+
+// #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE // doesnt work :(
+#define LOG_MQTT_LOCAL_LEVEL ESP_LOG_VERBOSE
 
 #define BUTTON_PIN 17
 
@@ -12,6 +16,8 @@ extern "C" {
 
 esp_mqtt_client_handle_t mqttClient;
 bool wifiStarted = false;
+bool mqttStarted = false;
+bool mqttConnected = false;
 
 static void log_error_if_nonzero(const char *message, int error_code)
 {
@@ -28,6 +34,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     int msg_id = 0;
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
+        mqttConnected = true;
         Serial.printf("MQTT_EVENT_CONNECTED\n");
         Serial.printf("mem: %d bytes\n", esp_get_free_heap_size());
         // msg_id = esp_mqtt_client_publish(client, "topic/qos1", "data_3", 0, 1, 0);
@@ -43,6 +50,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         // Serial.printf("sent unsubscribe successful, msg_id=%d\n", msg_id);
         break;
     case MQTT_EVENT_DISCONNECTED:
+        mqttConnected = false;
         Serial.printf("MQTT_EVENT_DISCONNECTED\n");
         break;
 
@@ -81,7 +89,7 @@ void setup() {
     Serial.begin(115200);
     delay(100);
 
-    Serial.println("\n========= BOOT INFO =========\n");
+    Serial.println("\n========= BOOT INFO =========");
     union {
         uint64_t val64;
         uint8_t val8[8];
@@ -96,24 +104,49 @@ void setup() {
     }
     Serial.printf("IDF version: %s\n", esp_get_idf_version());
     Serial.printf("Free memory: %d bytes\n", esp_get_free_heap_size());
-    Serial.println("\n=============================\n");
+    Serial.println("\n=============================");
 
     pinMode(BUTTON_PIN, INPUT_PULLUP);
 
     WiFi.mode(WIFI_MODE_STA);
 
+    esp_log_level_set("MQTT_CLIENT", LOG_MQTT_LOCAL_LEVEL);
+    esp_log_level_set("TRANS_SSL", LOG_MQTT_LOCAL_LEVEL);
+    esp_log_level_set("TRANSPORT_WS", LOG_MQTT_LOCAL_LEVEL);
+    esp_log_level_set("TRANSPORT_BASE", LOG_MQTT_LOCAL_LEVEL);
+    esp_log_level_set("esp-tls", LOG_MQTT_LOCAL_LEVEL);
+    esp_log_level_set("TRANSPORT", LOG_MQTT_LOCAL_LEVEL);
+    esp_log_level_set("OUTBOX", LOG_MQTT_LOCAL_LEVEL);
+
     esp_mqtt_client_config_t mqtt_cfg = {};
+    
+    // WSS
+    // mqtt_cfg.uri = "wss://host/ws";
+    // mqtt_cfg.cert_pem = (const char *)server_cert_pem_start;
+    
+    // WS
     mqtt_cfg.uri = "ws://host/ws";
+    
+    // MQTT
+    // mqtt_cfg.uri = "mqtt://host";
+    
     mqtt_cfg.client_id = "clientid";
     mqtt_cfg.buffer_size = 256;
     mqtt_cfg.network_timeout_ms = 5000;
     mqtt_cfg.keepalive = 60;
-    //mqtt_cfg.cert_pem = (const char *)server_cert_pem_start;
+
+    mqtt_cfg.lwt_topic = "lwt/clientid";
+    mqtt_cfg.lwt_msg = "offline";
+    mqtt_cfg.lwt_msg_len = 7;
+    mqtt_cfg.lwt_retain = false;
+    mqtt_cfg.lwt_qos = 0;
     
     mqttClient = esp_mqtt_client_init(&mqtt_cfg);
     Serial.printf("mqtt null %u\n", mqttClient == NULL);
     
-    esp_mqtt_client_register_event(mqttClient, (esp_mqtt_event_id_t)ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    if (mqttClient != NULL) {
+        esp_mqtt_client_register_event(mqttClient, (esp_mqtt_event_id_t)ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    }
 
     WiFi.begin("ssid", "password");
 }
@@ -130,9 +163,14 @@ void loop() {
             Serial.printf("wifconn mem: f=%d\n", esp_get_free_heap_size());
             delay(1000);
 
-            auto success = esp_mqtt_client_start(mqttClient);
-            Serial.printf("mqtt %d\n", success);
-            Serial.printf("mqttconn mem: f=%d\n", esp_get_free_heap_size());
+            if (mqttClient != NULL) {
+                auto success = esp_mqtt_client_start(mqttClient);
+                if (success == ESP_OK) {
+                    mqttStarted = true;
+                }
+                Serial.printf("mqtt %d\n", success);
+                Serial.printf("mqttconn mem: f=%d\n", esp_get_free_heap_size());
+            }
         }
         Serial.printf("connend mem: f=%d\n", esp_get_free_heap_size());
         return;
@@ -140,11 +178,28 @@ void loop() {
 
     delay(2000);
 
-    if (mqttClient != NULL && digitalRead(BUTTON_PIN) == LOW)
-    {
-        auto success = esp_mqtt_client_stop(mqttClient);
-        // auto success = esp_mqtt_client_disconnect(mqttClient);
-        Serial.printf("mqtt stop %d\n", success);
-        delay(3000);
+    if (mqttClient != NULL) {
+        if (digitalRead(BUTTON_PIN) == LOW) {
+            // source code: https://github.com/espressif/esp-mqtt/blob/36de30e46df5c04b2d633ceede6505905183489f/mqtt_client.c#L1579
+            esp_err_t success = esp_mqtt_client_disconnect(mqttClient);
+            Serial.printf("mqtt disconnnect %d\n", success);
+        }
+
+        if (mqttStarted && !mqttConnected) {
+            esp_err_t success;
+
+            // source code: https://github.com/espressif/esp-mqtt/blob/36de30e46df5c04b2d633ceede6505905183489f/mqtt_client.c#L1620
+            success = esp_mqtt_client_stop(mqttClient);
+            Serial.printf("mqtt stop %d\n", success);
+
+            success = esp_mqtt_client_destroy(mqttClient);
+            Serial.printf("mqtt destroy %d\n", success);
+            
+            mqttStarted = false;
+            mqttClient = NULL;
+
+            delay(3000);
+
+        }
     }
 }
